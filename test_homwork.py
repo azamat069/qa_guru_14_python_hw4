0c2c77923bcd4fc6bebeaf3ff4490744305028df
Azamat Abdurashidov <royalty5611@gmail.com>
2024-06-28 08:45:53 +0300

    fix

--- test_homwork.py
+++ test_homwork.py
@@ -58,7 +58,7 @@ def test_random_list():
     Создайте список из 10 случайных чисел от 1 до 100 (включая обе границы) и отсортируйте его по возрастанию.
     """
     # TODO создайте список
-    l = sample(range(1, 100), 10)
+    l = sample(range(1, 101), 10)
     l.sort()
     assert len(l) == 10
     assert all(l[i] <= l[i + 1] for i in range(len(l) - 1))
